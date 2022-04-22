76fa523762970fc99fbcea87a37613312bc51bbc
Doug A <douganderson444@gmail.com>
2022-04-22 08:56:47 -0300

    add timer (yikes.. 2 minutes?!)

--- sveltekit-rust-wasm/src/components/HelloWasm.svelte
+++ sveltekit-rust-wasm/src/components/HelloWasm.svelte
@@ -3,12 +3,17 @@
 
 	import init, { greet, generate_address } from 'wasm'
 
-	let address = 'TBD'
+	let address = 'Computing...'
+
 	onMount(async () => {
 		console.log('Initializing...')
 		await init()
-		console.log('Initialized!')
-		address = generate_address()
+		console.log('Generating...')
+		let start = Date.now()
+		// todo: put this in a worker so it doesn't block
+		address = generate_address('some seed value')
+		address += ` (${(Date.now() - start) / 1000}) secs`
+		console.log('Generated!')
 	})
 </script>
 
